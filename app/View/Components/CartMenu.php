19d51c8faff80ad09d80d4e8ffbd0c10ebbb35c9
mohamed awad <ma7amedawad@gmail.com>
2024-11-25 20:05:22 +0200

    Cart Menu component

---
+++ app/View/Components/CartMenu.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\View\Components;
+
+use App\Repositories\Cart\CartRepository;
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class CartMenu extends Component
+{
+    public $items;
+
+    public $total;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct(CartRepository $cart)
+    {
+        $this->items = $cart->get();
+        $this->total = $cart->total(); 
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.cart-menu');
+    }
+}
