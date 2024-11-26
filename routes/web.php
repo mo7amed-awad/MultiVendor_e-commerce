e172eeef751c70ff872d0d944ad63bfdb684278b
mohamed awad <ma7amedawad@gmail.com>
2024-11-26 08:16:20 +0200

    checkout and order

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Front\CartController;
+use App\Http\Controllers\Front\CheckoutController;
 use App\Http\Controllers\Front\HomeController;
 use App\Http\Controllers\Front\ProductsController;
 use App\Http\Controllers\ProfileController;
@@ -11,6 +12,8 @@ Route::get('/',[HomeController::class,'index'])->name('home');
 Route::get('/products',[ProductsController::class,'index'])->name('products.index');
 Route::get('/product/{product:slug}',[ProductsController::class,'show'])->name('product.show');
 Route::resource('cart',CartController::class);
+Route::get('checkout',[CheckoutController::class,'create'])->name('checkout');
+Route::post('checkout',[CheckoutController::class,'store']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
